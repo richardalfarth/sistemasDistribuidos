d3d6d298d9a2b8f53b092cdfc615143c335bc0f2
tiedt <ree_murilo@hotmail.com>
2021-04-20 23:01:57 -0300

    agora foi

---
+++ Trabalho3/frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedModule } from './_shared/_module/shared.module';
+import { EmpresaModule } from './empresa/empresa.module';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      DashboardComponent,
+      NavComponent,
+      DashboardComponent,
+   ],
+   imports: [
+      EmpresaModule,
+      SharedModule,
+      BrowserModule,
+      BrowserAnimationsModule,
+      AppRoutingModule,
+      HttpClientModule,
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
